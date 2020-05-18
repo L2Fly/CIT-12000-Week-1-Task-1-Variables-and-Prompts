4852c58ff6855242450dbe7a26f675c638eeaa2b
Laura Weyrick <65510690+L2Fly@users.noreply.github.com>
2020-05-18 17:15:17 -0400

    Add files via upload

---
+++ cit-12000-week-1-task-1-variables-and-prompts/README.markdown
@@ -0,0 +1,5 @@
+# CIT 12000 Week 1 Task 1 Variables and Prompts
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/l2fly/pen/ZEbmEvX](https://codepen.io/l2fly/pen/ZEbmEvX).
+
+
---
+++ cit-12000-week-1-task-1-variables-and-prompts/src/script.js
@@ -0,0 +1,8 @@
+var name = "Laura";
+var welcome = "CIT 12000";
+
+var greeting = "Hello there! My name is "+name;
+var welcome = "Welcome to "+welcome;
+
+document.querySelector(".words").innerHTML =greeting;
+document.querySelector(".words2").innerHTML =welcome;
